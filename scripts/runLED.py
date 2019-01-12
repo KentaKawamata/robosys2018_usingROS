092112239f598b921e04184c77e8d64e29a8907a
KentaKawamata <s16c1044sc@s.chibakoudai.jp>
2019-01-12 22:50:34 +0000

    add key

--- scripts/runLED.py
+++ scripts/runLED.py
@@ -3,21 +3,24 @@
 
 import roslib
 import rospy
+from std_msgs.msg import String
 import time
 import subprocess, os
 
-#main
+def Lchika(msg):
+
+    if msg.data == "1":
+        os.system("echo 1 > /dev/myled0")
+    elif msg.data == "2":
+        os.system("echo 2 > /dev/myled0")
+
+
 if __name__ == '__main__':
     ### init io port ###
     rospy.init_node('ledflash')
+    rospy.Subscriber('keys', String, Lchika)
 
-    while not rospy.is_shutdown():
+    rospy.spin()
 
-        os.system("echo 1 > /dev/myled0")
-        time.sleep(1)
-        os.system("echo 2 > /dev/myled0")
-        time.sleep(1)
-
-        
     os.system("echo 0 > /dev/myled0")
     print '\rStopped'
