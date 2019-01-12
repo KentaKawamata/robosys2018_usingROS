7a6efbc6ea719cc0a09a8342774724da1d12bf46
KentaKawamata <s16c1044sc@s.chibakoudai.jp>
2019-01-12 23:31:42 +0000

    add log in web

---
+++ scripts/webserver.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy, os
+import SimpleHTTPServer
+
+def kill():
+    
+    os.system("kill -KILL " + str(os.getpid())) #強制シャットダウン
+
+if __name__ == '__main__':
+
+    os.chdir(os.path.dirname(__file__))  #scriptsディレクトリが見えるように
+    rospy.init_node("webserver")         #rosのノード登録
+    rospy.on_shutdown(kill)              #kill関数の登録
+    SimpleHTTPServer.test()              #サーバ立ち上げ
